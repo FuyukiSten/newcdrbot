47b151e2dd90ed3d84506ef18c3152e1bcd25abe
FuyukiSten <41100594+FuyukiSten@users.noreply.github.com>
2018-11-30 01:32:40 -0200

    Update gay.js

--- comandos/gay.js
+++ comandos/gay.js
@@ -3,9 +3,7 @@ var waifu = require('snekfetch');
 
 exports.run = async (client, message, args) => {
     let gay = Math.round(Math.random() * 100);
-    if(message.channel.id !== "402216598666215427") {
-        return message.reply('utilize esse comando somente no canal <#402217817996263425>');
-    }
+  
     let gayembed = new Discord.RichEmbed()
         .setColor("#f442d4")
         .setTitle(`🏳 🌈 | **De acordo com meus calculos o usuário ${message.author.username} é ${gay}% gay!**`);
